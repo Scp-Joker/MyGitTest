014dd45e18f5398dae26fb027f8dec0d36e54355
L_Lawlite <1049927135@qq.com>
2021-10-03 16:19:17 +0800

    111

--- src/org/westos/demo/MyTest01.java
+++ src/org/westos/demo/MyTest01.java
@@ -3,5 +3,6 @@ package org.westos.demo;
 public class MyTest01 {
     public static void main(String[] args) {
         System.out.println("hello word!");
+        System.out.println("1111111");
     }
 }
